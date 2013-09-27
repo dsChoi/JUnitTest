3f0a00238b81fc3df76b0684939a55a71c63ddb8
dschoi <setin82@gmail.com>
2013-09-27 16:34:11 +0900

    commit

---
+++ src/main/java/com/spring/mvc/board/controller/Util.java
@@ -0,0 +1,15 @@
+package com.spring.mvc.board.controller;
+
+public class Util {
+
+	public static boolean isNull(Object obj) {
+
+		if (obj == null) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+}
